d13680005e70e4d44208575bb0d376877ea5cbca
Ciara Patterson <ciarapatterson@Ciaras-Macbook-Pro.local>
2022-03-23 15:50:43 -0600

    updated README.md, requirements, and models. added thesis and presentation

--- README.md
+++ README.md
@@ -2,6 +2,55 @@
 
 This repository contains the code, visualizations, presentations, final paper for my Gonzaga University Honors college senior thesis which investigated the effects of socioeconomic and behavioral factors on the spread of COVID-19. Some code is not included in this repository as it relied on private APIs or data repository that I was able to access from within Talus Analytics.
 
+# Table of contents
+
+- [reports](/reports)
+
+  - The paper, presentation, and figures created for this project
+
+  - [thesis](reports/Ciara_Patterson_Thesis_April_16_2021.pdf)
+
+  - [presentation](reports/Thesis_Presentation.pptx)
+
+  - [figures](/reports/figures)
+
+- [data](/data)
+
+  - The data used in this project. It's either in its raw form, edited manually, pre-processed (iterim), or processed completely for machine learning.
+
+  - [edited](/data/edited/)
+
+  - [interim](/data/intermin)
+
+  - [processed](/data/processed)
+
+  - [raw](/data/raw)
+
+- [notebooks](/notebooks)
+
+  - Jupter Notebooks created to analyze this data. One contains the final model and the other has exploratory data analysis.
+
+  - [exploratory_data_analysis.ipynb](/notebooks/exploratory_data_analysis.ipynb)
+
+  - [models.ipynb](/notebooks/models.ipynb)
+
+- [src](/src)
+
+  - Source code for creating the dataset, visualizations, and models
+
+  - _Still in the process of documentating according to Google Style_
+
+  - [data](/src/data)
+
+    - [visualize.py](/src/data/visualize.py)
+
+  - [models](/src/models)
+
+    - [train_and_test_model.py](/src/data/train_and_test_model.py)
+
+  - [visualization](/src/visualization)
+    - [visualize.py](/src/data/visualize.py)
+
 # Project goal
 
 In creating this project, I tried to understand why the success of policy restrictions had varied so widely across the US. No two regions appeared to respond to stay at home orders the same. Whereas, one county might see case sky-rocket following the closure of indoor dining, another might see them gradually fall. I suspected that this had to to with the both the willingness and the ability of people to comply with mitigation policy.
@@ -9,3 +58,24 @@ In creating this project, I tried to understand why the success of policy restri
 # Method
 
 To test this hypothesis, I identifed a set of counties with similar policy restrictions using [COVID-AMP](https://covidamp.org/). I then collected and combined a wide variety of data on that county's inhabitants including cell-phone mobility data, income, racial and ethnic identification, and teleworking ability. I then attempted to train a machine learning to predict the average caseload observed while the policy was in place given that information about the counties inhabitants.
+
+I ultimately used a random forest algorithm.
+
+![random-forest](reports/figures/tree.png)
+
+# Results
+
+To stop the model from using the time of year to estimate caseload, I split the larger dataset into a spring and fall dataset. On the spring dataset, the model was able to predict the number of COVID-19 cases observed while the policy was in place within an absolute mean error of 3.6 cases per 100,000 people, and the model was able to explain ~40-50% of the variance in the test set $(R^{2}=0.41, OOB = 0.48)$.
+
+![real-and-predicted](<reports/figures/real_and_predicted_values_Percentageracialminority(allpersonsexceptwhite,non-Hispanic)_scatter.png>)
+
+Race and household crowdedness were the two most important features in creating the final model prediction.
+![feature-importance](reports/figures/spring_feature_importance.png)
+
+From our exploratory data analysis, we know that those two features, like many of the features in our dataset are highly correlated, making it difficult to discern which was most important.
+
+![feature-heatmap](reports/figures/figure_heatmap.png)
+
+However, it seems likely that our model frequently relied on the racial makeup of a county in making its prediction in part because in the US race is highly correlated with access to healthcare, likelihood to be an essential worker, household crowdedness, and other factors that may affect COVID-19 outcomes.
+
+For a more in-depth discussion of the process and results, the final presentation and paper that resulted from this project have been included in reports.
--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,11 +1,19 @@
+from pyexpat import features
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 import pandas as pd
+import numpy as np 
+
 from sklearn import tree
 from sklearn.tree import export_graphviz
+
 from subprocess import call 
 from IPython.display import Image
+
 from types import MappingProxyType
+
+from scipy.stats import pearsonr
 import pydot
 
 # USED TO FORMAT FIGURES
@@ -27,7 +35,17 @@ PRETTY_NAMES = MappingProxyType(
                 'avg_new_cases':'Average new reported COVID-19 cases\nper 100,000 people'
             })
 
-def plot_feature_importance_bar_chart(feature_importance, PRETTY_NAMES, figure_name):
+def calculate_pvalues(df):
+    #https://stackoverflow.com/questions/25571882/pandas-columns-correlation-with-statistical-significance
+    df = df.dropna()._get_numeric_data()
+    dfcols = pd.DataFrame(columns=df.columns)
+    pvalues = dfcols.transpose().join(dfcols, how='outer')
+    for r in df.columns:
+        for c in df.columns:
+            pvalues[r][c] = pearsonr(df[r], df[c])[1]
+    return pvalues
+
+def plot_feature_importance_bar_chart(feature_importance, figure_name):
     sns.set(font_scale = 1.25, style = 'whitegrid')
     plt.figure(figsize = (8,5))
     ax = sns.barplot(x = 'importance', y = 'feature', data = feature_importance.replace(PRETTY_NAMES), palette = 'Greens_r')
@@ -43,10 +61,18 @@ def plot_feature_importance_bar_chart(feature_importance, PRETTY_NAMES, figure_n
 #  df_anc_pred.rename(columns = PRETTY_NAMES, inplace = True)
 
 def plot_real_predicted_scatter(preds_and_feats, figure_name):
+    
+    preds_and_feats = preds_and_feats.rename(columns = PRETTY_NAMES)
+
     for feat in preds_and_feats.columns:
-        plt.rc('font', size=12) 
+
+        plt.figure(figsize=(4,3))
+
+        # set font size and style
+        plt.rc('font', size=4) 
+        sns.set(font_scale=.5)
         plt.style.use('seaborn-whitegrid')
-        
+
         if feat == 'Start date of the policy environment':
             preds_and_feats[feat] = [np.datetime64(int(x), 'ns') for x in preds_and_feats[feat]]
             
@@ -54,16 +80,15 @@ def plot_real_predicted_scatter(preds_and_feats, figure_name):
         plt.scatter(preds_and_feats[feat], preds_and_feats.RRF_pred, c = 'r', alpha = 0.8, marker = '.', label = 'Predicted')
         
         title_str = 'Real and Predicted Average New COVID-19 Cases in the Test Dataset ($n$  = {})'
-        plt.title(title_str.format(len(preds_and_feats.index())))
+        plt.title(title_str.format(len(preds_and_feats.index)))
         
         plt.xlabel(feat)
         plt.ylabel('Average New COVID-19 Cases per 100k People')
         
         plt.grid(color = '#D3D3D3', linestyle = 'solid')
         plt.legend(loc = 'upper right', frameon = True)
-        
 
-        plt.savefig('reports/figures/' + figure_name + '_' + feat.replace('\n', ' ').replace('*', '') + '_scatter.png')
+        plt.savefig('reports/figures/' + figure_name + '_' + feat.replace(' ', '').replace('\n', '').replace('*', '') + '_scatter.png')
         plt.show()
         
 def plot_correlation_between_features(features_dataset, png_filename, r_filename, p_filename):
@@ -100,29 +125,42 @@ def plot_correlation_between_features(features_dataset, png_filename, r_filename
     plt.savefig(png_filename, bbox_inches="tight")
     
     
-def plot_correlation_coefficient(figure_name, features_w_anc):
-    sns.set(font_scale = 1.7, style = 'whitegrid')
-    plt.figure(figsize = (8,7))
+def plot_correlation_coefficient(features_w_anc, figure_name):
+    sns.set(font_scale = .7, style = 'whitegrid')
+    plt.figure(figsize = (6,5))
+
     corr_col = 'Correlation coefficient between feature\nand average new reported COVID-19 cases per 100,000 people'
     case_col = 'Average new reported COVID-19 cases\nper 100,000 people'
-    feat_corr_df = pd.DataFrame(columns = ['Feature', 'Correlation with average new cases'])
-    corr_data = features_w_anc.rename(columns = PRETTY_NAMES).corr().loc[case_col]
+
+    feat_corr_df = pd.DataFrame(columns = ['Feature', corr_col])
+    features_w_anc = features_w_anc.rename(columns = PRETTY_NAMES)
+    
+    corr_data = features_w_anc.corr().loc[case_col]
+    pvalue_data = calculate_pvalues(features_w_anc).loc[case_col]
+
     feat_corr_df['Feature'] = corr_data.index
     feat_corr_df[corr_col] = corr_data.tolist()
+    feat_corr_df['p-value'] = pvalue_data.tolist()
     feat_corr_df['Hue'] = np.where(feat_corr_df[corr_col] > 0, 
                                    'Postive correlation', 
                                    'Negative correlation')
+
+
     feat_corr_df = feat_corr_df[feat_corr_df['Feature'] != case_col]
+
     feat_corr_df.replace(PRETTY_NAMES, 
                          inplace = True)
+
     feat_corr_df = feat_corr_df[feat_corr_df['Feature'] != 'avg_new_cases']
+
     sns.set_palette(palette=['#6F1A07', '#1D4E89'])
     with sns.axes_style("whitegrid"):
         ax = sns.barplot(x = corr_col, y = 'Feature', 
                          hue = 'Hue', data = feat_corr_df)
         ax.get_legend().set_visible(False)
+
     # ax.set_title('Importance of features for estimating the percent days with decreasing new cases per 100k')
-    plt.savefig(figure_name, bbox_inches = 'tight')
+    plt.savefig('reports/figures/' + figure_name + '.png', bbox_inches = 'tight')
     return feat_corr_df
 
 def create_tree_estimator_png(regr_model, feature_list, estimator_n = 5):
